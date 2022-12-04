a4a3505601a29fe7b72ab7f5bfb51eafb0d2d012
Tony Sun <sunwuyue@live.com>
2022-12-04 15:59:13 -0500

    Day 4: 2022 Refractored

--- 2022/4/code.py
+++ 2022/4/code.py
@@ -7,10 +7,8 @@ with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
 
 cnt = 0
 for line in input:
-    first, second = line.strip().split(',')
-    l1,r1 = first.split('-')
-    l2,r2 =  second.split('-')
-    l1, l2, r1, r2 = int(l1), int(l2), int(r1), int(r2)
+    l, r = [x.split('-') for x in line.strip().split(',')]
+    l1, l2, r1, r2 = int(l[0]), int(r[0]), int(l[1]), int(r[1])
     if ((l1 <= l2 and r2 <= r1) or # range 2 is inner
         (l2 <= l1 and r1 <= r2)): # range 1 is inner
         cnt += 1
@@ -19,12 +17,10 @@ print("Part One : "+ str(cnt))
 
 cnt = 0
 for line in input:
-    first, second = line.strip().split(',')
-    l1,r1 = first.split('-')
-    l2,r2 =  second.split('-')
-    l1, l2, r1, r2 = int(l1), int(l2), int(r1), int(r2)
-    if ((l1 <= l2 and r1 >= l2) or # range 2 is inner
-        (l2 <= l1 and r2 >= l1)): # range 1 is inner
+    l, r = [x.split('-') for x in line.strip().split(',')]
+    l1, l2, r1, r2 = int(l[0]), int(r[0]), int(l[1]), int(r[1])
+    if ((l1 <= l2 and r1 >= l2) or # end of first range overlaps with second
+        (l2 <= l1 and r2 >= l1)):  # end of second range overlaps with first
         cnt += 1
 
 print("Part Two : "+ str(cnt))
