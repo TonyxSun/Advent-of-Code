8af4e1a6f5457f6bec5963f0f8456c31fa281909
Tony Sun <sunwuyue@live.com>
2022-12-14 17:04:36 -0500

    Day 13: 2022 - code is getting >.<

--- 2022/13/code.py
+++ 2022/13/code.py
@@ -2,14 +2,76 @@
 # Author = TonyxSun
 # Link = https://adventofcode.com/2022/day/13
 # Date = December 13, 2022
-
+import collections, functools, bisect
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.readlines()
 
 # ---------------------------- Part One ----------------------------
+def addToList(idx, str) -> tuple:
+    res = collections.deque()
+    while idx < len(str):
+        if str[idx] == "[":
+            newList = addToList(idx+1, str)
+            idx = newList[0]
+            res.append(newList[1])
+        elif str[idx] == "]":
+            return (idx, res)
+        elif str[idx].isnumeric():
+            s = str[idx]
+            if str[idx + 1].isnumeric():
+                s += str[idx + 1]
+                idx += 1
+            res.append(int(s))
+        idx += 1
+    return (len(str),res)
 
-print("Part One : "+ str(None))
+def compare_lists(vals1, vals2) ->int:
+    while vals1 and vals2:
+
+        val1 = vals1.popleft()
+        val2 = vals2.popleft()
+        if isinstance(val1, int) and isinstance(val2, int):
+            if val1 < val2:
+                return 1
+            elif val1 > val2:
+                return -1
+        else:
+            val1 = collections.deque([val1]) if isinstance(val1, int) else val1
+            val2 = collections.deque([val2]) if isinstance(val2, int) else val2
+
+            subList = compare_lists(val1, val2)
+            if 0 != subList:
+                return subList
+
+    if vals1:
+        return -1
+    return 1 if vals2 else 0
+
+def check_in_order(str1, str2):
+    q1, q2 =addToList(0, str1)[1], addToList(0, str2)[1]
+    return compare_lists(q1, q2)
+
+res = 0
+idx = 0
+lines = []
+for i in range(0, len(input), 3):
+    idx += 1
+    if check_in_order(input[i].strip(), input[i +1].strip()):
+        res += idx
+        lines.append(input[i].strip())
+        lines.append(input[i+1].strip())
+
+print("Part One : "+ str(res))
 
 # ---------------------------- Part Two ----------------------------
 
-print("Part Two : "+ str(None))
+lines.append('[[2]]')
+lines.append('[[6]]')
+lines.sort(key=functools.cmp_to_key(check_in_order),reverse=True)
+
+res = 1
+for i, line in enumerate(lines):
+    if line == '[[2]]' or line == '[[6]]':
+        res *= (i + 1)
+
+print("Part Two : "+ str(res))
